8e591bff1619127f1b354316085ea7d168fb7f2c
mateuszcygan <mateuszcygan@protonmail.com>
2024-09-16 16:16:17 +0200

    The latest version of this task written on entwicklerheld.de

--- task.py
+++ task.py
@@ -23,13 +23,25 @@ def check_guess(secret_word, guess):
 
 def guess_word(word_list, word_length, feedback_history):
 
+    num_of_words = len(word_list)
+    x = len(feedback_history)
+
     if len(feedback_history) == 0:
-
-        # dictionary - letter at the correct position ('G'), list - letters that the secret word includes
-        secret_word = {}
-        secret_letters = []
-
-        for i in word_length:
-            secret_word[i] = None
-
         return word_list[0]
+
+    # Extract the latest feedback word and its feedback
+    feedback_word = feedback_history[-1][0]
+    feedback = feedback_history[-1][1]
+
+    feedback_word_length = len(feedback_word)
+
+    # For each 'word' in the 'word_list'
+    # Compare the letters of both words - taken word and word with feedback
+    # If it is the same, depending on the feedback letters, decide what to do with words
+    # THINK HOW TO AVOID COMPARING THE SAME WORD and in which cases it can occur
+    for feedback_letter in feedback:
+        for word in word_list:
+            return
+
+
+guess_word(["anana", "apple"], 5, [("apple", ["G", "G", "X", "G", "X"])])
